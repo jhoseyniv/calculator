9c8f623ce5932376221dcc9799176df9c65e631d
98912 <jhoseyniv@gmail.com>
2023-03-16 19:46:57 +0330

    add unit test methods

--- src/test/java/com/test/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/test/calculator/CalculatorApplicationTests.java
@@ -3,11 +3,15 @@ package com.test.calculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CalculatorApplicationTests {
+	private Calculator calculator = new Calculator();
 
 	@Test
 	void contextLoads() {
+		assertEquals(5,calculator.sum(2,3));
 	}
 
 }
